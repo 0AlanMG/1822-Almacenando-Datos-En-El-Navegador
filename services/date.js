91c9a3298b3d32d95b7e03f1436f2e2dac984285
Alan Montes de Oca Garcia <esc.amdog5@gmail.com>
2022-11-15 14:36:26 -0600

    Proyecto Completado

---
+++ services/date.js
@@ -0,0 +1,20 @@
+export const uniqueDates = (tasks) => {
+    const unique = [];
+    
+    tasks.forEach(tasks => {
+        if(!unique.includes(tasks.dateFormat))
+            unique.push(tasks.dateFormat);
+    });
+
+    return unique;
+};
+
+
+export const orderDates = (dates) => {
+    return dates.sort((a, b) => {
+        const firstDate = moment(a, "DD/MM/YYYY");
+        const secondDate = moment(b, "DD/MM/YYYY");
+
+        return firstDate - secondDate;
+    });
+};
